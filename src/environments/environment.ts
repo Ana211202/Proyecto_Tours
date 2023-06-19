722b63bb034650f87828bd4e83b1288dc0a786de
kevinsavala <118188106+kevinsavala@users.noreply.github.com>
2023-06-19 01:27:10 -0500

    borrado de backend y cambio de base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'proyectotours-81d94',
-    appId: '1:307191822237:web:8badb9ff5bda1f1bf1a174',
-    databaseURL: 'https://proyectotours-81d94-default-rtdb.firebaseio.com',
-    storageBucket: 'proyectotours-81d94.appspot.com',
-    apiKey: 'AIzaSyBdBiv0JChuRg49RK839CnWkW8KeigwCss',
-    authDomain: 'proyectotours-81d94.firebaseapp.com',
-    messagingSenderId: '307191822237',
-    measurementId: 'G-BY0C0B0SP3',
+    projectId: 'proyectotours-93653',
+    appId: '1:582523168482:web:b86dc1151cee57aa8ee0bb',
+    databaseURL: 'https://proyectotours-93653-default-rtdb.firebaseio.com',
+    storageBucket: 'proyectotours-93653.appspot.com',
+    apiKey: 'AIzaSyAi3Iym4JwbbfiNuB23lGGPJKWUsZwmRQk',
+    authDomain: 'proyectotours-93653.firebaseapp.com',
+    messagingSenderId: '582523168482',
+    measurementId: 'G-0DFSKCC5D6',
   },
 };
