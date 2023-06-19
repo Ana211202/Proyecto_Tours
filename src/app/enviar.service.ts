fedf210dc68d9a256e7a7381a9fccf6d2ba62db1
thenataliam <nataliamartinez.32@outlook.com>
2023-06-18 19:33:09 -0600

    servicio envío de correo formulario de contacto

---
+++ src/app/enviar.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnviarService {
+
+  private url="http://localhost:3000/envio";
+
+  constructor(private http:HttpClient) { }
+
+  sendEmail(_body: { email: any; }) {
+    return this.http.post(this.url,_body);
+    
+  }
+}
