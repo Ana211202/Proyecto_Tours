228c0808b68d291a9231b26edf67f28e63c55656
kevinsavala <118188106+kevinsavala@users.noreply.github.com>
2023-06-19 04:54:48 -0600

    reserva funcionando

--- src/app/componentes/navbar/navbar.component.spec.ts
+++ src/app/componentes/navbar/navbar.component.spec.ts
@@ -1,28 +1,4 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-@Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
-})
-
-export class NavbarComponent {
-  searchTerm: string;
-
- constructor(private router: Router) {
-  this.searchTerm = ''; // Asignar un valor en el constructor
-}
-
-  buscar() {
-    if (this.searchTerm) {
-      this.router.navigate(['/search-results'], { queryParams: { q: this.searchTerm } });
-    }
-  }
-}
-
-/*import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -44,4 +20,3 @@ describe('NavbarComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-*/
